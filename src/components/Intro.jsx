24d5d9d01ad4776ad70928abfa5d6eb14c9eb095
Lim Hyeon taek <hunteak1020@gmail.com>
2026-07-06 18:37:08 +0900

    프로젝트 주제(화학식 계산 시각화)소개 및 컴포넌트 추가

---
+++ src/components/Intro.jsx
@@ -0,0 +1,14 @@
+function Intro() {
+  return (
+    <div style={{ textAlign: "center", padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>🧪 화학식 계산 시각화</h1>
+      <p>화학식을 입력하면 원소 개수와 분자량을 계산하고,</p>
+      <p>이를 시각적으로 보여주는 프로젝트입니다.</p>
+      <p style={{ color: "gray", fontSize: "14px" }}>
+        (주제는 변경될 수 있습니다)
+      </p>
+    </div>
+  );
+}
+
+export default Intro;
