9d5eb2bacef8478d4e3f3894290d855a3f4fd9cc
신주하 <ladea1224@pusan.ac.kr>
2026-07-06 19:19:05 +0900

    react_ project intro

---
+++ src/ProjectIntro.jsx
@@ -0,0 +1,15 @@
+function ProjectIntro() {
+  return (
+    <section className="project-intro">
+      <h1>hub</h1>
+      <p>간단한 소개를 담은 프로젝트입니다.</p>
+      <ul>
+        <li>React로 구성된 프론트엔드</li>
+        <li>Vite 기반 개발 환경</li>
+        <li>추후 내용이 채워질 예정입니다</li>
+      </ul>
+    </section>
+  )
+}
+
+export default ProjectIntro
