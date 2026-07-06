871500b471e24ef6d830af8b64a06a56469c2b07
naruv0134 <lliniano@gmail.com>
2026-07-06 19:19:23 +0900

    feat: 프로젝트 소개 UI 추가

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ProjectIntro from "./components/ProjectIntro";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <ProjectIntro />
+    </main>
+  );
+}
+
+export default App;
