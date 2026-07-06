fee458543ac5e3674f239dbb197782e56a5070b0
trudy_0 <csy31244@naver.com>
2026-07-06 20:20:25 +0900

    [N173_최서영] 위치기반 공동구매 시스템

---
+++ Project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// 1. 방금 만든 컴포넌트를 불러옵니다 (import)
+import ProjectIntro from "./ProjectIntro";
+
+function App() {
+  return (
+    <div>
+      {/* 2. 불러온 컴포넌트를 화면에 배치합니다 */}
+      <ProjectIntro />
+    </div>
+  );
+}
+
+export default App;
