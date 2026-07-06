69335791f6391e05af781242315c38fd1248f095
Mureung1 <ruxxer30@gmail.com>
2026-07-06 22:38:24 +0900

    CRA 방식을 활용한 React 환경 세팅

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div
+      style={{ textAlign: "center", padding: "50px", fontFamily: "sans-serif" }}
+    >
+      <h1>안녕하세요! N162_조정민입니다.</h1>
+      <p>
+        이 프로젝트는 React의 전통적인 환경 구성 방식(CRA)을 탐구하기 위한
+        워밍업 미션입니다.
+      </p>
+      <p>기초부터 탄탄히 다져가겠습니다!</p>
+    </div>
+  );
+}
+
+export default App;
