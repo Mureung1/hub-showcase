92196a6dd6cb9291d938767aee299ae8919c6f83
윤태용 <dbsxodyd001@naver.com>
2026-07-06 18:28:38 +0900

    feat: add SmartFF Agent React intro page

---
+++ web/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <header className="header">
+      <span className="header-brand">SmartFF Agent</span>
+    </header>
+  )
+}
+
+export default Header
