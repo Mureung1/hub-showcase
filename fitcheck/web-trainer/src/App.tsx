885b5d68cf24801a5e50d5590b5e115a9db60ff0
xlouisjungx <cutaandcrazy@gmail.com>
2026-07-07 17:11:59 +0900

    README에 FitCheck 기획서를 작성한 노션링크 추가

--- fitcheck/web-trainer/src/App.tsx
+++ fitcheck/web-trainer/src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <p>회원 관리, 운동 프로그램, 피드백을 한곳에서 관리하세요.</p>
       </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
