9c54d4bb16304a7c7ec7ccde8c00a1d76d07717f
Junghan Pyo <vywjdgks131344@gmail.com>
2026-07-07 15:47:06 +0900

    react-router-dom 설치 및 기본 라우팅 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import CategoryPage from './pages/CategoryPage'
+import RecipeDetailPage from './pages/RecipeDetailPage'
 
 function App() {
-  return <Home />
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/category/:categoryId" element={<CategoryPage />} />
+      <Route path="/recipe/:recipeId" element={<RecipeDetailPage />} />
+    </Routes>
+  )
 }
 
 export default App
---
+++ src/pages/CategoryPage.jsx
@@ -0,0 +1,5 @@
+function CategoryPage() {
+  return <main className="min-h-screen bg-orange-50 px-4 py-10">카테고리 상세 (준비 중)</main>
+}
+
+export default CategoryPage
