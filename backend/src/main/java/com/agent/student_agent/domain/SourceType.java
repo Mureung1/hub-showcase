32f88045a3d6435adbfebbf65de3968dba3d31ac
khy05300@gmail.com <khy05300@gmail.com>
2026-07-09 23:37:04 +0900

    뼈대 갈아엎음

--- backend/src/main/java/com/agent/student_agent/domain/SourceType.java
+++
@@ -1,9 +0,0 @@
-package com.agent.student_agent.domain;
-
-public enum SourceType {
-    SCHOOL_NOTICE,
-    DEPT_NOTICE,
-    LMS,
-    SCHOLARSHIP,
-    CONTEST
-}
--- backend/src/main/java/com/agent/student_agent/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.agent.student_agent.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String email;
-    private String password;
-}
