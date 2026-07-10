dc17139f0780abda47a9f9cfb2f374634343e95c
uncledrew <bonggutravel@gmail.com>
2026-07-10 15:26:16 +0900

    feat: add front-end setting

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import ProjectIntro from './components/ProjectIntro';
-
-function App() {
-  return <ProjectIntro />;
-}
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Briefy</h1>
+      <p>개발 환경 세팅 완료 ✓</p>
+    </div>
+  );
+}
+
+export default App;
