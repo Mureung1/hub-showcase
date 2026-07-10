ac57e6040e3853322046081f39c9ebace5e69d01
geunnseung <bsg1017@gmail.com>
2026-07-10 20:39:04 +0900

    chore: 백엔드 Spring Boot 프로젝트 구성

---
+++ backend/src/main/java/com/chasewar/ChasewarApplication.java
@@ -0,0 +1,13 @@
+package com.chasewar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChasewarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChasewarApplication.class, args);
+    }
+
+}
