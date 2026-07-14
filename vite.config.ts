9a7d1a018542d3bf48408954762a61b64e10a159
hoon-ground <break_the_limit@naver.com>
2026-07-14 17:35:33 +0900

    Chore: src 절대경로 별칭 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
@@ -7,6 +8,11 @@ export default defineConfig({
       jsxImportSource: '@emotion/react',
     }),
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   test: {
     css: true,
     environment: 'jsdom',
