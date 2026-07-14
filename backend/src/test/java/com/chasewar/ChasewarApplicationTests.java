965b2d668e1f0b43454b7310be0583b91dc13fb6
geunnseung <bsg1017@gmail.com>
2026-07-14 15:36:14 +0900

    chore: Testcontainers 기반 통합 테스트 환경 구성

--- backend/src/test/java/com/chasewar/ChasewarApplicationTests.java
+++ backend/src/test/java/com/chasewar/ChasewarApplicationTests.java
@@ -1,10 +1,9 @@
 package com.chasewar;
 
+import com.chasewar.support.IntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ChasewarApplicationTests {
+class ChasewarApplicationTests extends IntegrationTest {
 
     @Test
     void contextLoads() {
