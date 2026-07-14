3f3b92da324bfba97860aa73dad1458565fb8db8
hoon-ground <break_the_limit@naver.com>
2026-07-14 17:29:05 +0900

    Chore: React 프론트엔드 개발 환경 구성

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+  ],
+  test: {
+    css: true,
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/test/setup.ts',
+  },
+})
