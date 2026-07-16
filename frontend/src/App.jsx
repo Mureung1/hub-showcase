f04baa7d26896cfc211ecbd8064a8e6c59ba6706
hyunjinch <hjin24@pusan.ac.kr>
2026-07-16 17:28:58 +0900

    feat: 메인 페이지 임시 구현 및 공통 레이아웃 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Layout from './layouts/Layout';
+import Home from './pages/Home';
 import ProjectInfo from './pages/ProjectInfo';
 import LoginPage from './pages/LoginPage';
 import OAuthCallback from './pages/OAuthCallback';
@@ -9,12 +10,12 @@ function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<Navigate to="/about" replace />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<ProjectInfo />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/oauth/callback" element={<OAuthCallback />} />
-      <Route path="*" element={<NotFound />} />
     </Routes>
 
   );
