c411f127d19f39ade60f42684797140e127e7157
Junsub Lee <lee980605@knu.ac.kr>
2026-07-21 17:18:13 +0900

    chore: 웹 개발 환경과 Tailwind 설정 추가

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./styles/tailwind.css";
 import "./style.css";
+import "./styles/modal.css";
 import { App } from "./App";
 
 const rootElement = document.getElementById("root");
