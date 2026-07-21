d3e769338715f88b632fbb40825d307e46fb8dc7
hyunjinch <hjin24@pusan.ac.kr>
2026-07-21 20:48:34 +0900

    feat: 로그인 페이지를 상단바 버튼으로 대체

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -1,9 +1,16 @@
+import { useEffect, useState } from 'react'
 import { Outlet, Link, useLocation } from 'react-router-dom';
+import { fetchMe } from '../lib/auth'
 import './Layout.css'
 
 const Layout = () => {
   const location = useLocation()
   const isSubscriptionFormPage = location.pathname === '/subscriptions/new'
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    fetchMe().then(setUser)
+  }, [])
 
   return (
     <div className="app-layout">
@@ -11,11 +18,22 @@ const Layout = () => {
         <Link to="/" className="gnb-logo">
             SUBZIP
         </Link>
-        {!isSubscriptionFormPage && (
-          <Link to="/subscriptions/new" className="gnb-cta">
-            + 구독 서비스 등록
-          </Link>
-        )}
+        <div className="gnb-actions">
+          {!isSubscriptionFormPage && (
+            <Link to="/subscriptions/new" className="gnb-cta">
+              + 구독 서비스 등록
+            </Link>
+          )}
+          {user ? (
+            <Link to="/mypage" className="gnb-login">
+              마이페이지
+            </Link>
+          ) : (
+            <a href="/api/auth/google" className="gnb-login">
+              로그인
+            </a>
+          )}
+        </div>
       </header>
 
       <main className="frame-body">
