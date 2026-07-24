b574bb63d37d5d150c0ea3efd12b4f17d5a75b25
SWH <sungwook3596@gmail.com>
2026-07-24 19:24:11 +0900

    chore: remove public landing workspace

--- apps/landing/src/index.ts
+++
@@ -1,7 +0,0 @@
-export {
-  LandingReleaseDisplayError,
-  decodeLandingReleaseDisplay,
-  type LandingReleaseDisplay,
-  type LandingReleaseLink,
-} from './release-display.js'
-export { renderLandingDocument } from './render.js'
