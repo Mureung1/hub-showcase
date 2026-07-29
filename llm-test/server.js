159b3713e2514a673ac4ead9f18e1cfc96a7204b
dalum <bini420813@gmail.com>
2026-07-29 17:12:03 +0900

    용량 수정 코드 원본으로 복귀

--- llm-test/server.js
+++ llm-test/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "50mb" })); // 요청 body를 JSON으로 읽을 수 있게 해줌
+app.use(express.json()); // 요청 body를 JSON으로 읽을 수 있게 해줌
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({
