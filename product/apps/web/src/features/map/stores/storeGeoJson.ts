95f9b58a10a9db031f393f0f71b431a476223ba0
HyunKN <wjdg159@gmail.com>
2026-07-30 20:19:51 +0900

    feat(web): add store hover layer id

--- product/apps/web/src/features/map/stores/storeGeoJson.ts
+++ product/apps/web/src/features/map/stores/storeGeoJson.ts
@@ -7,6 +7,7 @@ export const STORE_CLUSTER_COUNT_LAYER_ID = "localtwin-store-cluster-counts";
 export const STORE_POINT_LAYER_ID = "localtwin-store-points-visible";
 export const STORE_CATEGORY_ICON_LAYER_ID = "localtwin-store-category-icons";
 export const STORE_POINT_HIT_LAYER_ID = "localtwin-store-points-hit";
+export const STORE_HOVER_HALO_LAYER_ID = "localtwin-store-hover-halo";
 export const STORE_SELECTED_HALO_LAYER_ID = "localtwin-store-selected-halo";
 export const STORE_SELECTED_POINT_LAYER_ID = "localtwin-store-selected-point";
 
